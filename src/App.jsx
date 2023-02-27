00c349fd25ba0d0219429decc03f0b6b066110c8
matheus <matheusaltrao2@gmail.com>
2023-02-27 18:39:03 -0400

    add darkmode in card

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,72 @@
+import { useEffect, useState } from 'react';
 import { AiOutlineInstagram } from 'react-icons/ai';
 import { AiOutlineLinkedin } from 'react-icons/ai';
 import { AiOutlineGithub } from 'react-icons/ai';
 
 function App() {
 
+  const [theme, setTheme] = useState(localStorage.theme)
+
+  useEffect(() => {
+
+    if (theme === 'dark') {
+
+      localStorage.setItem('theme', 'dark')
+      document.documentElement.classList.add('dark')
+
+    } else {
+      document.documentElement.classList.remove('dark')
+      localStorage.removeItem('theme', 'dark')
+    }
+
+  }, [theme])
+
+  function handleSwitch() {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
 
   return (
-    <div className="container h-screen w-screen mx-auto px-4 flex items-center justify-center ">
+    <div id='app' className=" w-screen h-screen   px-4 flex items-center justify-center dark:bg-gray-800 ">
 
-      <div className=" animate-top flex flex-col bg-gray-900 border border-sky-500 shadow-lg shadow-sky-800 py-4 px-6 rounded-md" >
+      <div className=" animate-top flex flex-col bg-white border border-purple-600  shadow-purple-500  py-4 px-6 rounded-md relative  shadow-lg   dark:bg-gray-900 dark:text-white dark:shadow-cyan-500 dark:border-cyan-600
+        " /*    */ >
+
+        <label className="absolute block items-center justify-center cursor-pointer top-4 right-4 outline-none   ">
+          <input onClick={handleSwitch} type="checkbox" value="" className="sr-only peer outline-none " />
+          <div className=" w-[55px] h-6  rounded-full peer  peer-checked:after:translate-x-[30px] peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all bg-purple-600 peer-checked:border-gray-600 dark:bg-cyan-400
+            " >
+            {/*   */}
+
+
+          </div>
+        </label>
+
 
         <div className="flex flex-row items-center ">
-          <div className=" overflow-hidden rounded-full border-2 border-sky-500">
+          <div className=" overflow-hidden rounded-full border-2 border-purple-600 dark:border-sky-500     "> {/**/}
 
-            <img className="w-32 object-cover hover:scale-150 ease duration-300 " src="https://github.com/matheusaltrao.png" alt="" />
+            <img className="w-32 object-cover " src="https://github.com/matheusaltrao.png" alt="" />
 
           </div>
           <div className="ml-4 ">
-            <h3 className="font-semibold text-xl">Matheus Altrão</h3>
-            <h4 className="text-indigo-300 pt-1">Front End Developer</h4>
+            <h3 className="font-semibold text-xl ">Matheus Altrão</h3>
+            <h4 className=" font-light opacity-70 pt-1">Front-End </h4>
           </div>
 
         </div>
 
         <div className="mt-6">
-          <h2 className="font-bold text-cyan-400  mb-2 text-lg">Bio</h2>
-          <p className="text-gray-300 font-normal mt-1 max-w-sm text-left" >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur voluptatum autem asperiores accusamus id nisi et officiis dolores? Natus suscipit corporis veritatis ullam animi neque praesentium nisi optio ipsa voluptatibus.
+          <h2 className="font-bold text-purple-500 mb-2 text-lg dark:text-cyan-400  ">Bio</h2> {/*   */}
+          <p className=" font-medium opacity-80 mt-1 max-w-sm text-left" >
+            Sou um programador Front-end apaixonado e motivado, com
+            experiência em várias ferramentas e tecnologias populares, como
+            ReactJs,TailwindCss.
           </p>
         </div>
 
         <div className="mt-6">
-          <h2 className="font-bold text-cyan-400  mb-2 text-lg">Techs</h2>
+          <h2 className="font-bold text-purple-500  mb-2 text-lg dark:text-cyan-400    ">Techs</h2> {/* dark:text-cyan-400  */}
 
           <div className="mt-4">
             <ul className="list-disc pl-5">
@@ -44,12 +79,11 @@ function App() {
         </div>
 
         <div className="mt-6">
-          <h2 className="font-bold text-cyan-400 mb-2 text-lg" >Social</h2>
+          <h2 className="font-bold text-purple-500  mb-2 text-lg dark:text-cyan-400   ">Social</h2> {/* dark:text-cyan-400  */}
 
           <ul className='flex items-center justify-left gap-4'>
-            <li className='text-2xl'><a target={'_blank'} href="#"> <AiOutlineInstagram /> </a></li>
-            <li className='text-2xl'><a target={'_blank'} href="#"> <AiOutlineLinkedin /> </a></li>
-            <li className='text-2xl'><a target={'_blank'} href="#"> <AiOutlineGithub /> </a></li>
+            <li className='text-2xl'><a target={'_blank'} href="https://www.linkedin.com/in/matheus-altrao/"> <AiOutlineLinkedin /> </a></li>
+            <li className='text-2xl'><a target={'_blank'} href="https://github.com/MatheusAltrao"> <AiOutlineGithub /> </a></li>
           </ul>
         </div>
 
